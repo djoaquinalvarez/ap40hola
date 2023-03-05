5305fc4343e9aff3306399af9bcf7653bd804b81
Francisco Grana <126683454+Franciscograna@users.noreply.github.com>
2023-03-05 17:30:35 -0300

    Create Clase3Ejercicio2b.java

---
+++ Clase3Ejercicio2b.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+ 
+ 
+ String abecedario = "abcdefghijklmnñopqrstuvwxyz";
+ String codificar ="hola que tal";
+ String salida="";
+ int desplazamiento = 2;
+ 
+ for (int i=0; i<codificar.length(); i++)
+        {    
+
+    
+            
+        char indice = codificar.charAt(i);
+        
+        
+                   
+        
+        int defase = abecedario.indexOf(indice)+desplazamiento;
+ 
+        salida = salida + abecedario.charAt(defase);
+             
+                
+        }
+ 
+ 
+ 
+        
+        System.out.println(salida);
+    }
+}

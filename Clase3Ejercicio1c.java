063d2b4837549cff47f0a5673bf100720bfa7ab3
Francisco Grana <126683454+Franciscograna@users.noreply.github.com>
2023-03-05 10:57:05 -0300

    Create Clase3Ejercicio1c.java

---
+++ Clase3Ejercicio1c.java
@@ -0,0 +1,30 @@
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        
+        char letra ;
+        string str ;
+        int numerosin[] ;
+        int x ;
+        int count =0;
+        
+
+     
+       
+                  for (int i=0; i< numeros[].length(); i++ );{
+        
+            if ( numerosin[i] > numerosin[i+1]);{
+                
+              count++
+            }
+        }            
+ 
+    
+
+        System.out.println(numeros);
+    }
+}

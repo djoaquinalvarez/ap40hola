55589d6b2dd41c72e44fad35e34597c262758abb
Francisco Grana <126683454+Franciscograna@users.noreply.github.com>
2023-03-05 15:28:42 -0300

    Update Clase3Ejercicio1a.java

--- Clase3Ejercicio1a.java
+++ Clase3Ejercicio1a.java
@@ -9,9 +9,9 @@ public class Main {
         int count = 0;
 
         
-        for (int i = 0; i< str.length(); i++ );{
+        for (int i = 0; i< str.length(); i++ )  {
         
-            if (str.charAt(i) = "a") {
+            if (str.charAt(i) == letra) {
                 count++;
             }
         }

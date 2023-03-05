e91db05796a4177556b38d10224eeb8808c528c3
Francisco Grana <126683454+Franciscograna@users.noreply.github.com>
2023-03-05 10:49:52 -0300

    Create Clase3Ejercicio1b.java
    8 err

---
+++ Clase3Ejercicio1b.java
@@ -0,0 +1,44 @@
+
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        
+        char letra ;
+        string str ;
+        int numerosin[] = new int[3];
+        int numeros[] = new int[3];
+        boolean orden;
+        
+    if (orden = true) { 
+     
+       
+                  for (int i=0; i< numeros[].length(); i++ );{
+        
+            if ( numerosin[i] < numerosin[i+1]);{
+                
+                numeros[i] = numeros[i];
+                numeros[i+1] = numeros[i+1];
+            }
+        }            
+    }
+    
+    
+    else {    
+        
+                   for (int i=0; i< numeros[].length(); i++ );{
+        
+            if ( numerosin[i] > numerosin[i+1]);{
+                
+                numeros[i] = numeros[i];
+                numeros[i+1] = numeros[i+1];
+            }
+        }  
+      
+    }
+        System.out.println(numeros);
+    }
+}

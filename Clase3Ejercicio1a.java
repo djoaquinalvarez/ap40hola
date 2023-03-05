8cb7e96fc1cd036d15de1d67bb33fa01afa8380d
Francisco Grana <126683454+Franciscograna@users.noreply.github.com>
2023-03-05 10:22:12 -0300

    Create Clase3Ejercicio1a.java
    entrega, error en for

---
+++ Clase3Ejercicio1a.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)  {
+        
+        
+        char letra = 'a' ;
+        String str = "argentina";
+        int count = 0;
+
+        
+        for (int i = 0; i< str.length(); i++ );{
+        
+            if (str.charAt(i) = "a") {
+                count++;
+            }
+        }
+        
+        
+        System.out.println(count);
+    }
+}

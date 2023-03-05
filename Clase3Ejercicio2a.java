89e70052c802867d31476f43da8a28367ed7156b
Francisco Grana <126683454+Franciscograna@users.noreply.github.com>
2023-03-05 17:27:24 -0300

    Update Clase3Ejercicio2a.java

--- Clase3Ejercicio2a.java
+++ Clase3Ejercicio2a.java
@@ -10,26 +10,20 @@ public class Main {
  int desplazamiento = 1;
  
  for (int i=0; i<codificar.length(); i++)
+     
+     
         {    
 
-    
             
         char indice = codificar.charAt(i);
-        
-        
-        if (indice.equals(" ")) 
        
-        else
-                    {
         
         int defase = abecedario.indexOf(indice)+desplazamiento;
  
         salida = salida + abecedario.charAt(defase);
              
-                }
+                
         }
- 
- 
  
         
         System.out.println(salida);

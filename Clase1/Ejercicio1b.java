d13de486b9b131842e5586d3e05e752723f300d7
Francisco Grana <126683454+Franciscograna@users.noreply.github.com>
2023-03-05 22:06:31 -0300

    Create Ejercicio1b.java

---
+++ Clase1/Ejercicio1b.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        int numeroInicio = 5;
+        int numeroFin = 14;
+        
+while( (numeroInicio <= numeroFin)  ){
+
+ System.out.println( numeroInicio );
+ 
+ numeroInicio = numeroInicio +1;
+
+}
+      
+    }
+}

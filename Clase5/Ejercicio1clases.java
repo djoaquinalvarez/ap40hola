232448259769230a04c88337675f02dc2ca72e38
Francisco Grana <126683454+Franciscograna@users.noreply.github.com>
2023-03-14 15:16:14 -0300

    Create Ejercicio1clases.java

---
+++ Clase5/Ejercicio1clases.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		package E1;
+		
+		import java.time.localdate;
+		
+		public class CarritodCompras{
+		  private String cliente;
+		  private localdate fechacompra;
+		  private double precio;
+		  private String item1;
+                  private String item1;
+                  private String item1;
+		}
+		
+		
+		
+		public class producto {
+		  private String nombreprod;
+		  private double precio;
+		  private int cantidad;
+    
+		}
+		
+		
+	}
+}

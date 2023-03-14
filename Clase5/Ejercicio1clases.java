b0b03598a28355f803bf106a7d4e4b534f54a102
Francisco Grana <126683454+Franciscograna@users.noreply.github.com>
2023-03-14 15:38:56 -0300

    Update Ejercicio1clases.java

--- Clase5/Ejercicio1clases.java
+++ Clase5/Ejercicio1clases.java
@@ -1,36 +1,37 @@
-import java.util.*;
-
-public class Main {
-	public static void main(String[] args) {
-		package E1;
-		
-		import java.time.localdate;
-		
-		public class CarritodCompras{
-		  private String cliente;
-		  private localdate fechacompra;
-		  private double precio;
-		  private String item1;
-          private String item1;
-          private String item1;
-		}
-		
-		
-		
-		public class producto {
-		  private String nombreprod;
-		  private double precio;
-		  private int cantidad;
+public class CarritoCompras {
+    private String cliente;
+    private LocalDate fechaCompra;
+    private double precio;
+    private String item1;
+    private String item2;
+    private String item3;
     
-    		
-			public class descuento {
-		  private boolean aplica;
-    
-		}
-		}
-
-
-	}
+    public CarritoCompras(String cliente, LocalDate fechaCompra, double precio, String item1, String item2, String item3) {
+        this.cliente = cliente;
+        this.fechaCompra = fechaCompra;
+        this.precio = precio;
+        this.item1 = item1;
+        this.item2 = item2;
+        this.item3 = item3;
+    }
 }
 
-
+public class Producto {
+    private String nombreProd;
+    private double precio;
+    private int cantidad;
+    
+    public Producto(String nombreProd, double precio, int cantidad) {
+        this.nombreProd = nombreProd;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+    
+    public static class Descuento {
+        private boolean aplica;
+        
+        public Descuento(boolean aplica) {
+            this.aplica = aplica;
+        }
+    }
+}

bf1556fe0b71104adcc9efb26e4e6d595cd7c437
Francisco Grana <126683454+Franciscograna@users.noreply.github.com>
2023-03-22 16:51:25 -0300

    Create CalculadoraTest.java

---
+++ Clase6/src/Modelo/CalculadoraTest.java
@@ -0,0 +1,48 @@
+package Modelo;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+
+public class CalculadoraTest {
+	@Test
+    public void testCalcularResta() {
+        Calculadora calc = new Calculadora(90, 50);
+        double resultado = 15*calc.calcularResta();
+        assertEquals(605, resultado, 0.0001);          
+    }
+	
+	@Test
+    public void testCalcularSuma() {
+		
+        Calculadora calc = new Calculadora(150, 180);   
+        double resultado = calc.calcularSuma()/3;
+        assertEquals(110, resultado, 0.0001);        
+    }
+	@Test
+    public void testCalcularProd() {
+        Calculadora calc = new Calculadora(80, 3);
+        double resultado = calc.calcularProd();
+        assertEquals(240, resultado, 0.0001);
+    }
+	@Test
+    public void testCalcularDiv() {
+        Calculadora calc = new Calculadora(150, 180);
+        double resultado = calc.calcularDiv()/3;
+        assertNotEquals(110, resultado, 0.0001);
+    }
+	@Test
+    public void testCalcularSuma2() {
+		
+        Calculadora calc = new Calculadora(70, 40);
+        
+        double resultado = calc.calcularSuma()*25;
+        assertNotEquals(2700, resultado, 0.0001);        
+    }
+	
+	
+	
+	
+	
+	

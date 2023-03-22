ab37313748c3d5bcbb73b1b716834c2499c8b942
Joaquin Alvarez <71903725+djoaquinalvarez@users.noreply.github.com>
2023-03-22 17:59:32 -0300

    Observaciones Guía N°5

--- Clase5/CarritoApp.java
+++ Clase5/CarritoApp.java
@@ -36,9 +36,20 @@ public class CarritoApp {
 			double precio = Double.parseDouble(linea.split(";" )[2]);
 			
 			Producto producto = new Producto (nombre, precio);
+			/*
+			Joaquín Alvarez: Fran, soy Joaquin, el auxiliar del curso. No lo ejecuté, pero me parece que la linea de abajo te debería
+			
+			dar un error, porque en el constructor de ItemCompra declaraste que se pasa como parametro un array de productos,
+			
+			y acá estas pasando una instancia de la clase Producto. Revisalo, por las dudas. Te recomiendo usar el constructor vacío,
+			
+			y luego setear cada uno de los atributos del objeto con los métodos seters.
+			*/
+			
 			ItemCompra itemCompra = new ItemCompra( producto, cantidad); 
+			
 		
-			compra.getItems()[i] = itemCompra;
+			compra.getItems()[i] = itemCompra; //Joaquín Alvarez: Acá es mejor usar el método set.
 			i++;
 			
 			
